b8a25f06ad54ff98e8ab122f4accff89476e69f3
uttues <458594033@qq.com>
2020-07-01 21:40:32 +0800

    7.1 阅读注释

--- packages/carousel/src/item.vue
+++ packages/carousel/src/item.vue
@@ -6,132 +6,174 @@
       'is-active': active,
       'el-carousel__item--card': $parent.type === 'card',
       'is-in-stage': inStage,
-      'is-hover': hover,
+      'is-hover': hover,ElCarouselItem
       'is-animating': animating
     }"
     @click="handleItemClick"
-    :style="itemStyle">
-    <div
+    :style="itemStyle"
+  >
+    <!-- 卡片化才有 -->
+    <!-- <div
       v-if="$parent.type === 'card'"
       v-show="!active"
-      class="el-carousel__mask">
-    </div>
+      class="el-carousel__mask"
+    >
+    </div> -->
     <slot></slot>
   </div>
 </template>
 
 <script>
-  import { autoprefixer } from 'element-ui/src/utils/util';
-  const CARD_SCALE = 0.83;
-  export default {
-    name: 'ElCarouselItem',
+import { autoprefixer } from "element-ui/src/utils/util";
+const CARD_SCALE = 0.83;
+export default {
+  name: "ElCarouselItem",
 
-    props: {
-      name: String,
-      label: {
-        type: [String, Number],
-        default: ''
-      }
-    },
-
-    data() {
-      return {
-        hover: false,
-        translate: 0,
-        scale: 1,
-        active: false,
-        ready: false,
-        inStage: false,
-        animating: false
-      };
-    },
-
-    methods: {
-      processIndex(index, activeIndex, length) {
-        if (activeIndex === 0 && index === length - 1) {
-          return -1;
-        } else if (activeIndex === length - 1 && index === 0) {
-          return length;
-        } else if (index < activeIndex - 1 && activeIndex - index >= length / 2) {
-          return length + 1;
-        } else if (index > activeIndex + 1 && index - activeIndex >= length / 2) {
-          return -2;
-        }
-        return index;
-      },
-
-      calcCardTranslate(index, activeIndex) {
-        const parentWidth = this.$parent.$el.offsetWidth;
-        if (this.inStage) {
-          return parentWidth * ((2 - CARD_SCALE) * (index - activeIndex) + 1) / 4;
-        } else if (index < activeIndex) {
-          return -(1 + CARD_SCALE) * parentWidth / 4;
-        } else {
-          return (3 + CARD_SCALE) * parentWidth / 4;
-        }
-      },
-
-      calcTranslate(index, activeIndex, isVertical) {
-        const distance = this.$parent.$el[isVertical ? 'offsetHeight' : 'offsetWidth'];
-        return distance * (index - activeIndex);
-      },
-
-      translateItem(index, activeIndex, oldIndex) {
-        const parentType = this.$parent.type;
-        const parentDirection = this.parentDirection;
-        const length = this.$parent.items.length;
-        if (parentType !== 'card' && oldIndex !== undefined) {
-          this.animating = index === activeIndex || index === oldIndex;
-        }
-        if (index !== activeIndex && length > 2 && this.$parent.loop) {
-          index = this.processIndex(index, activeIndex, length);
-        }
-        if (parentType === 'card') {
-          if (parentDirection === 'vertical') {
-            console.warn('[Element Warn][Carousel]vertical direction is not supported in card mode');
-          }
-          this.inStage = Math.round(Math.abs(index - activeIndex)) <= 1;
-          this.active = index === activeIndex;
-          this.translate = this.calcCardTranslate(index, activeIndex);
-          this.scale = this.active ? 1 : CARD_SCALE;
-        } else {
-          this.active = index === activeIndex;
-          const isVertical = parentDirection === 'vertical';
-          this.translate = this.calcTranslate(index, activeIndex, isVertical);
-        }
-        this.ready = true;
-      },
-
-      handleItemClick() {
-        const parent = this.$parent;
-        if (parent && parent.type === 'card') {
-          const index = parent.items.indexOf(this);
-          parent.setActiveItem(index);
-        }
-      }
-    },
-
-    computed: {
-      parentDirection() {
-        return this.$parent.direction;
-      },
-
-      itemStyle() {
-        const translateType = this.parentDirection === 'vertical' ? 'translateY' : 'translateX';
-        const value = `${translateType}(${ this.translate }px) scale(${ this.scale })`;
-        const style = {
-          transform: value
-        };
-        return autoprefixer(style);
-      }
-    },
-
-    created() {
-      this.$parent && this.$parent.updateItems();
-    },
-
-    destroyed() {
-      this.$parent && this.$parent.updateItems();
+  props: {
+    name: String,
+    label: {
+      type: [String, Number],
+      default: ""
     }
-  };
+  },
+
+  data() {
+    return {
+      hover: false,
+      translate: 0,
+      scale: 1,
+      active: false,
+      ready: false,
+      inStage: false,
+      animating: false
+    };
+  },
+
+  methods: {
+    processIndex(index, activeIndex, length) {
+      // 当前是activeIndex是第一张，index是最后一张，返回-1，相差-1，表示二者相邻且index在左侧
+      if (activeIndex === 0 && index === length - 1) {
+        return -1;
+      } else if (activeIndex === length - 1 && index === 0) {
+        // 当前页activeIndex是最后一张，index是第一张，返回length，相差1，表示二者相邻且index在右侧
+        return length;
+        // 如果，index在activeIndex前一页的前面，并且之间的间隔在一半页数即以上，则返回页数长度+1，这样它们会被置于最右侧
+      } else if (index < activeIndex - 1 && activeIndex - index >= length / 2) {
+        return length + 1;
+        // 如果，index在activeIndex后一页的后面，并且之间的间隔在一般页数即以上，则返回-2，这样它们会被置于最左侧
+      } else if (index > activeIndex + 1 && index - activeIndex >= length / 2) {
+        return -2;
+      }
+      return index;
+    },
+
+    calcCardTranslate(index, activeIndex) {
+      const parentWidth = this.$parent.$el.offsetWidth;
+      if (this.inStage) {
+        return (
+          (parentWidth * ((2 - CARD_SCALE) * (index - activeIndex) + 1)) / 4
+        );
+      } else if (index < activeIndex) {
+        return (-(1 + CARD_SCALE) * parentWidth) / 4;
+      } else {
+        return ((3 + CARD_SCALE) * parentWidth) / 4;
+      }
+    },
+
+    /**
+     * 当activeIndex从3变成4，index=0的元素translate = -3*width => -4*width（左移）
+     */
+    calcTranslate(index, activeIndex, isVertical) {
+      const distance = this.$parent.$el[
+        isVertical ? "offsetHeight" : "offsetWidth"
+      ];
+      return distance * (index - activeIndex);
+    },
+
+    /**
+     * 需要轮播时，由外层carousel调用translateItem，触发元素移动
+     * @param index 当前carousel-item索引
+     * @param activeIndex 轮播需要显示在正中间的carousel-item索引
+     * @param oldIndex 过去的activeIndex
+     * 所有元素都会重新计算自己的translate值，生成动态样式
+     * (列表中只有两个元素需要移动，旧的activeIndex（左移移走），新的activeIndex（左移移入）？？？)
+     */
+    translateItem(index, activeIndex, oldIndex) {
+      const parentType = this.$parent.type;
+      const parentDirection = this.parentDirection;
+      // 图片个数
+      const length = this.$parent.items.length;
+
+      // 如果不是card模式，修改当前状态（是否移动）
+      if (parentType !== "card" && oldIndex !== undefined) {
+        this.animating = index === activeIndex || index === oldIndex;
+      }
+
+      // 循环播放
+      if (index !== activeIndex && length > 2 && this.$parent.loop) {
+        // 处理当前索引
+        index = this.processIndex(index, activeIndex, length);
+      }
+
+      // 更新当前元素active状态（是否为activeIndex），计算当前元素的Translate并修改 => 触发新的style计算，动态样式
+      if (parentType === "card") {
+        if (parentDirection === "vertical") {
+          console.warn(
+            "[Element Warn][Carousel]vertical direction is not supported in card mode"
+          );
+        }
+        this.inStage = Math.round(Math.abs(index - activeIndex)) <= 1;
+        this.active = index === activeIndex;
+        this.translate = this.calcCardTranslate(index, activeIndex);
+        this.scale = this.active ? 1 : CARD_SCALE;
+      } else {
+        this.active = index === activeIndex;
+        const isVertical = parentDirection === "vertical";
+        this.translate = this.calcTranslate(index, activeIndex, isVertical);
+      }
+      this.ready = true;
+    },
+
+    /**
+     * 在card模式下点击切换
+     */
+    handleItemClick() {
+      const parent = this.$parent;
+      if (parent && parent.type === "card") {
+        const index = parent.items.indexOf(this);
+        parent.setActiveItem(index);
+      }
+    }
+  },
+
+  computed: {
+    /**
+     * 计算属性，返回el-carousel上的type属性【不用多次写】
+     */
+    parentDirection() {
+      return this.$parent.direction;
+    },
+
+    /**
+     * translate的值发生改变就会自动执行，计算样式，返回一个style对象，动态样式
+     */
+    itemStyle() {
+      const translateType =
+        this.parentDirection === "vertical" ? "translateY" : "translateX";
+      const value = `${translateType}(${this.translate}px) scale(${this.scale})`;
+      const style = {
+        transform: value
+      };
+      return autoprefixer(style);
+    }
+  },
+
+  created() {
+    this.$parent && this.$parent.updateItems();
+  },
+
+  destroyed() {
+    this.$parent && this.$parent.updateItems();
+  }
+};
 </script>
--- packages/carousel/src/main.vue
+++ packages/carousel/src/main.vue
@@ -2,64 +2,77 @@
   <div
     :class="carouselClasses"
     @mouseenter.stop="handleMouseEnter"
-    @mouseleave.stop="handleMouseLeave">
+    @mouseleave.stop="handleMouseLeave"
+  >
+    <!-- 箭头 -->
     <div
       class="el-carousel__container"
-      :style="{ height: height }">
-      <transition
-        v-if="arrowDisplay"
-        name="carousel-arrow-left">
-        <button
-          type="button"
-          v-show="(arrow === 'always' || hover) && (loop || activeIndex > 0)"
-          @mouseenter="handleButtonEnter('left')"
-          @mouseleave="handleButtonLeave"
-          @click.stop="throttledArrowClick(activeIndex - 1)"
-          class="el-carousel__arrow el-carousel__arrow--left">
-          <i class="el-icon-arrow-left"></i>
-        </button>
-      </transition>
-      <transition
-        v-if="arrowDisplay"
-        name="carousel-arrow-right">
-        <button
-          type="button"
-          v-show="(arrow === 'always' || hover) && (loop || activeIndex < items.length - 1)"
-          @mouseenter="handleButtonEnter('right')"
-          @mouseleave="handleButtonLeave"
-          @click.stop="throttledArrowClick(activeIndex + 1)"
-          class="el-carousel__arrow el-carousel__arrow--right">
-          <i class="el-icon-arrow-right"></i>
-        </button>
-      </transition>
+      :style="{ height: height }"
+    >
+      <!-- <transition
+                v-if="arrowDisplay"
+                name="carousel-arrow-left"
+            >
+                <button
+                    type="button"
+                    v-show="(arrow === 'always' || hover) && (loop || activeIndex > 0)"
+                    @mouseenter="handleButtonEnter('left')"
+                    @mouseleave="handleButtonLeave"
+                    @click.stop="throttledArrowClick(activeIndex - 1)"
+                    class="el-carousel__arrow el-carousel__arrow--left"
+                >
+                    <i class="el-icon-arrow-left"></i>
+                </button>
+            </transition>
+            <transition
+                v-if="arrowDisplay"
+                name="carousel-arrow-right"
+            >
+                <button
+                    type="button"
+                    v-show="(arrow === 'always' || hover) && (loop || activeIndex < items.length - 1)"
+                    @mouseenter="handleButtonEnter('right')"
+                    @mouseleave="handleButtonLeave"
+                    @click.stop="throttledArrowClick(activeIndex + 1)"
+                    class="el-carousel__arrow el-carousel__arrow--right"
+                >
+                    <i class="el-icon-arrow-right"></i>
+                </button>
+            </transition> -->
       <slot></slot>
     </div>
-    <ul
-      v-if="indicatorPosition !== 'none'"
-      :class="indicatorsClasses">
-      <li
-        v-for="(item, index) in items"
-        :key="index"
-        :class="[
+    <!-- 下面一行的指示器 -->
+    <!-- <ul
+            v-if="indicatorPosition !== 'none'"
+            :class="indicatorsClasses"
+        >
+            <li
+                v-for="(item, index) in items"
+                :key="index"
+                :class="[
           'el-carousel__indicator',
           'el-carousel__indicator--' + direction,
           { 'is-active': index === activeIndex }]"
-        @mouseenter="throttledIndicatorHover(index)"
-        @click.stop="handleIndicatorClick(index)">
-        <button class="el-carousel__button">
-          <span v-if="hasLabel">{{ item.label }}</span>
-        </button>
-      </li>
-    </ul>
+                @mouseenter="throttledIndicatorHover(index)"
+                @click.stop="handleIndicatorClick(index)"
+            >
+                <button class="el-carousel__button">
+                    <span v-if="hasLabel">{{ item.label }}</span>
+                </button>
+            </li>
+        </ul> -->
   </div>
 </template>
 
 <script>
-import throttle from 'throttle-debounce/throttle';
-import { addResizeListener, removeResizeListener } from 'element-ui/src/utils/resize-event';
+import throttle from "throttle-debounce/throttle";
+import {
+  addResizeListener,
+  removeResizeListener
+} from "element-ui/src/utils/resize-event";
 
 export default {
-  name: 'ElCarousel',
+  name: "ElCarousel",
 
   props: {
     initialIndex: {
@@ -69,7 +82,7 @@ export default {
     height: String,
     trigger: {
       type: String,
-      default: 'hover'
+      default: "hover"
     },
     autoplay: {
       type: Boolean,
@@ -86,7 +99,7 @@ export default {
     },
     arrow: {
       type: String,
-      default: 'hover'
+      default: "hover"
     },
     type: String,
     loop: {
@@ -95,9 +108,9 @@ export default {
     },
     direction: {
       type: String,
-      default: 'horizontal',
+      default: "horizontal",
       validator(val) {
-        return ['horizontal', 'vertical'].indexOf(val) !== -1;
+        return ["horizontal", "vertical"].indexOf(val) !== -1;
       }
     }
   },
@@ -114,45 +127,60 @@ export default {
 
   computed: {
     arrowDisplay() {
-      return this.arrow !== 'never' && this.direction !== 'vertical';
+      return this.arrow !== "never" && this.direction !== "vertical";
     },
 
     hasLabel() {
       return this.items.some(item => item.label.toString().length > 0);
     },
 
+    /**
+     * 根据direction、type，动态生成类名 el-carousel el-carousel--card
+     */
     carouselClasses() {
-      const classes = ['el-carousel', 'el-carousel--' + this.direction];
-      if (this.type === 'card') {
-        classes.push('el-carousel--card');
+      const classes = ["el-carousel", "el-carousel--" + this.direction];
+      if (this.type === "card") {
+        classes.push("el-carousel--card");
       }
       return classes;
     },
 
     indicatorsClasses() {
-      const classes = ['el-carousel__indicators', 'el-carousel__indicators--' + this.direction];
+      const classes = [
+        "el-carousel__indicators",
+        "el-carousel__indicators--" + this.direction
+      ];
       if (this.hasLabel) {
-        classes.push('el-carousel__indicators--labels');
+        classes.push("el-carousel__indicators--labels");
       }
-      if (this.indicatorPosition === 'outside' || this.type === 'card') {
-        classes.push('el-carousel__indicators--outside');
+      if (this.indicatorPosition === "outside" || this.type === "card") {
+        classes.push("el-carousel__indicators--outside");
       }
       return classes;
     }
   },
 
   watch: {
+    /**
+     * items发生改变，就将外层传入的initialIndex设置为ActiveItem
+     */
     items(val) {
       if (val.length > 0) this.setActiveItem(this.initialIndex);
     },
 
+    /**
+     * 如果activeIndex的值发生改变，就会调用resetItemPosition，传入旧值，调整所有carousel-item位置
+     */
     activeIndex(val, oldVal) {
       this.resetItemPosition(oldVal);
       if (oldVal > -1) {
-        this.$emit('change', val, oldVal);
+        this.$emit("change", val, oldVal);
       }
     },
 
+    /**
+     * 根据是否自动播放的值，来决定开启、关闭定时器
+     */
     autoplay(val) {
       val ? this.startTimer() : this.pauseTimer();
     },
@@ -163,30 +191,42 @@ export default {
   },
 
   methods: {
+    /**
+     * 鼠标移动到轮播图上时，不转动
+     */
     handleMouseEnter() {
       this.hover = true;
       this.pauseTimer();
     },
-
     handleMouseLeave() {
       this.hover = false;
       this.startTimer();
     },
 
+    /**
+     * 根据是否在边界上，返回'left||right||false'
+     */
     itemInStage(item, index) {
       const length = this.items.length;
-      if (index === length - 1 && item.inStage && this.items[0].active ||
-        (item.inStage && this.items[index + 1] && this.items[index + 1].active)) {
-        return 'left';
-      } else if (index === 0 && item.inStage && this.items[length - 1].active ||
-        (item.inStage && this.items[index - 1] && this.items[index - 1].active)) {
-        return 'right';
+      if (
+        (index === length - 1 && item.inStage && this.items[0].active) ||
+        (item.inStage && this.items[index + 1] && this.items[index + 1].active)
+      ) {
+        return "left";
+      } else if (
+        (index === 0 && item.inStage && this.items[length - 1].active) ||
+        (item.inStage && this.items[index - 1] && this.items[index - 1].active)
+      ) {
+        return "right";
       }
       return false;
     },
 
+    /**
+     * 鼠标是否悬浮在arrow上，arrow = 'left||right'
+     */
     handleButtonEnter(arrow) {
-      if (this.direction === 'vertical') return;
+      if (this.direction === "vertical") return;
       this.items.forEach((item, index) => {
         if (arrow === this.itemInStage(item, index)) {
           item.hover = true;
@@ -195,22 +235,33 @@ export default {
     },
 
     handleButtonLeave() {
-      if (this.direction === 'vertical') return;
+      if (this.direction === "vertical") return;
       this.items.forEach(item => {
         item.hover = false;
       });
     },
 
+    /**
+     * el-carousel内除了el-carousel-item可能还有其他子组件，这里做一层过滤
+     */
     updateItems() {
-      this.items = this.$children.filter(child => child.$options.name === 'ElCarouselItem');
+      this.items = this.$children.filter(
+        child => child.$options.name === "ElCarouselItem"
+      );
     },
 
+    /**
+     * 根据传入的参数oldIndex，遍历调整所有的carousel-item的位置
+     */
     resetItemPosition(oldIndex) {
       this.items.forEach((item, index) => {
         item.translateItem(index, this.activeIndex, oldIndex);
       });
     },
 
+    /**
+     * 轮播（activeIndex+1||=0）
+     */
     playSlides() {
       if (this.activeIndex < this.items.length - 1) {
         this.activeIndex++;
@@ -226,25 +277,34 @@ export default {
       }
     },
 
+    /**
+     * 定时器，每隔this.interval就滚一次
+     */
     startTimer() {
       if (this.interval <= 0 || !this.autoplay || this.timer) return;
       this.timer = setInterval(this.playSlides, this.interval);
     },
 
+    /**
+     * 手动切换幻灯片（根据name属性/索引）=> 是否需要修改activeIndex => 触发resetItemPosition，通知item位置变化
+     */
     setActiveItem(index) {
-      if (typeof index === 'string') {
+      // name属性 => 索引
+      if (typeof index === "string") {
         const filteredItems = this.items.filter(item => item.name === index);
         if (filteredItems.length > 0) {
           index = this.items.indexOf(filteredItems[0]);
         }
       }
+      // 索引
       index = Number(index);
       if (isNaN(index) || index !== Math.floor(index)) {
-        console.warn('[Element Warn][Carousel]index must be an integer.');
+        console.warn("[Element Warn][Carousel]index must be an integer.");
         return;
       }
       let length = this.items.length;
       const oldIndex = this.activeIndex;
+      // 如果传入的index超出列表边界，就根据“是否循环播放”来设置activeIndex值，会自动触发resetItemPosition
       if (index < 0) {
         this.activeIndex = this.loop ? length - 1 : 0;
       } else if (index >= length) {
@@ -252,6 +312,7 @@ export default {
       } else {
         this.activeIndex = index;
       }
+      // 手动触发resetItemPosition
       if (oldIndex === this.activeIndex) {
         this.resetItemPosition(oldIndex);
       }
@@ -270,7 +331,7 @@ export default {
     },
 
     handleIndicatorHover(index) {
-      if (this.trigger === 'hover' && index !== this.activeIndex) {
+      if (this.trigger === "hover" && index !== this.activeIndex) {
         this.activeIndex = index;
       }
     }
@@ -285,6 +346,9 @@ export default {
     });
   },
 
+  /**
+   * 过滤掉非carouser-item元素，在nextTick中，添加resize监听器，初始化activeIndex后，开启定时器
+   */
   mounted() {
     this.updateItems();
     this.$nextTick(() => {
